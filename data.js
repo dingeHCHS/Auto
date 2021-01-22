fe6a800ef4dc0f93833bf56e551181f0752f462f
skyblaster89 <skyblaster89@gmail.com>
2021-01-22 13:44:33 -0600

    Auto

---
+++ data.js
@@ -0,0 +1,91 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-auto",
+      "name": "Auto",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.30714540297627835,
+          "pitch": -0.19531992706059143,
+          "title": "Auto Intro",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.1301041469592885,
+          "pitch": 0.009852413498837365,
+          "title": "Auto 1&nbsp;",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.1143170518754317,
+          "pitch": 0.07199095732585548,
+          "title": "Auto 2",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.0399320746420884,
+          "pitch": -0.030946535415150578,
+          "title": "Auto 3",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.956676390459542,
+          "pitch": -0.13825230564855495,
+          "title": "Auto 4",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.8712723270802059,
+          "pitch": -0.24120650975778446,
+          "title": "Auto 5",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.6598053519073996,
+          "pitch": -0.1496504805176997,
+          "title": "Auto 6",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5752251059917732,
+          "pitch": -0.3418791154436356,
+          "title": "Auto 7",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Auto",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
