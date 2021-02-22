7f1567fc9e9fa5ffabf5f452c42116969e9fedb9
dingeHCHS <69469881+dingeHCHS@users.noreply.github.com>
2021-02-22 11:21:53 -0600

    asd

---
+++ data.js
@@ -0,0 +1,91 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-auto",
+      "name": "Auto",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.30714540297627835,
+          "pitch": -0.19531992706059143,
+          "title": "Auto Intro",
+          "text": '<iframe src="https://drive.google.com/file/d/1Gh_Ee9fr7QrstUtEZ4S8ilgwB3UuBlhb/preview" width="640" height="480"></iframe>'
+        },
+        {
+          "yaw": -0.1301041469592885,
+          "pitch": 0.009852413498837365,
+          "title": "Auto 1&nbsp;",
+          "text": '<iframe src="https://drive.google.com/file/d/1Gs6y36Y2glyUhoIzcCZc_BBUk-rYMz2G/preview" width="640" height="480"></iframe>'
+        },
+        {
+          "yaw": -2.1143170518754317,
+          "pitch": 0.07199095732585548,
+          "title": "Auto 2",
+          "text": '<iframe src="https://drive.google.com/file/d/1Gv9T04SvqFCPk49A6wj7JdRJ5LOP226N/preview" width="640" height="480"></iframe>'
+        },
+        {
+          "yaw": -2.0399320746420884,
+          "pitch": -0.030946535415150578,
+          "title": "Auto 3",
+          "text": '<iframe src="https://drive.google.com/file/d/1Gz8DEDbm2R3Pf1PJNGu832ETfTi-Efek/preview" width="640" height="480"></iframe>'
+        },
+        {
+          "yaw": -1.956676390459542,
+          "pitch": -0.13825230564855495,
+          "title": "Auto 4",
+          "text": '<iframe src="https://drive.google.com/file/d/1H2dNFe5hfjE4MCnaHQ9_YrABqzoiJf3X/preview" width="640" height="480"></iframe>'
+        },
+        {
+          "yaw": -1.8712723270802059,
+          "pitch": -0.24120650975778446,
+          "title": "Auto 5",
+          "text": '<iframe src="https://drive.google.com/file/d/1HDzVx2gana_Ro8kd0TYAmZofwKDLKzyJ/preview" width="640" height="480"></iframe>'
+        },
+        {
+          "yaw": -0.6598053519073996,
+          "pitch": -0.1496504805176997,
+          "title": "Auto 6",
+          "text": '<iframe src="https://drive.google.com/file/d/1HRqwjaPUAgoAfcY1c-D7Sg3gE4RmvrMI/preview" width="640" height="480"></iframe>'
+        },
+        {
+          "yaw": -0.5752251059917732,
+          "pitch": -0.3418791154436356,
+          "title": "Auto 7",
+          "text": '<iframe src="https://drive.google.com/file/d/1HXaD8vag0SrNzfrM505W3wO9sDxC4zgI/preview" width="640" height="480"></iframe>'
+        }
+      ]
+    }
+  ],
+  "name": "Auto",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
